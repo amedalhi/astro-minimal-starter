dd94d45632290654412ce77c43b8d1bc1b2a7d7b
amedalhi <100897687+amedalhi@users.noreply.github.com>
2025-05-12 16:51:29 -0700

    feat: theme switcher with cookies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: node({
+    mode: "standalone",
+  }),
+  output: "server",
+  session: {
+    driver: "redis",
+  },
+  experimental: {
+    session: true,
+  },
+});
